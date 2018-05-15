4fbda2b4ad2eaddfad00dbda48860a66ba739c1c
juan.lage@gmail.com <juan.lage@gmail.com>
2018-05-15 15:02:51 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a test of GCP Datastore based on https://github.com/kbxkb/google-cloud-datastore-php
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y update
 sudo apt-get -y install apache2 php5 php5-memcached memcached
 sudo apt-get -y install git
-git clone https://github.com/kbxkb/google-cloud-datastore-php.git
+git clone https://github.com/nilloBE/temp-tests
 cd google-cloud-datastore-php
 sudo chmod -R 777 /var/www
 sudo cp calldatastore.php /var/www/html
