fa6597d217a58b6e35d3fa4ef084bbf5d89eef81
juan.lage@gmail.com <juan.lage@gmail.com>
2018-05-15 15:31:13 +0200

    added credentials

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,8 @@ sudo chmod -R 777 /var/www
 sudo cp checkdatastore.php /var/www/html
 sudo mv /var/www/html/index.html /var/www/html/index.html.bak
 sudo cp index.html /var/www/html
+sudo mkdir $HOME/credentials
+sudo touch $HOME/credentials/web-autocomplete-01-e910d561d520.json
 sudo bash -c "echo 'GOOGLE_APPLICATION_CREDENTIALS=/home/juan_lage/credentials/web-autocomplete-01-e910d561d520.json' >> /etc/environment"
 cd /var/www/html
 sudo bash -c "curl -sS https://getcomposer.org/installer | php"
