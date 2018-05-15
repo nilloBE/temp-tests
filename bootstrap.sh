c85cb4b15c831011f6bd9a3d06efa418c3231215
juan.lage@gmail.com <juan.lage@gmail.com>
2018-05-15 15:05:38 +0200

    first test

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,11 @@ sudo apt-get -y update
 sudo apt-get -y install apache2 php5 php5-memcached memcached
 sudo apt-get -y install git
 git clone https://github.com/nilloBE/temp-tests
-cd google-cloud-datastore-php
+cd tempt-tests
 sudo chmod -R 777 /var/www
-sudo cp calldatastore.php /var/www/html
-sudo cp form.html /var/www/html
+sudo cp checkdatastore.php /var/www/html
+sudo mv /var/www/html/index.html /var/www/html/index.html.bak
+sudo cp index.html /var/www/html
 sudo bash -c "echo 'GOOGLE_APPLICATION_CREDENTIALS=/home/juan_lage/credentials/web-autocomplete-01-e910d561d520.json' >> /etc/environment"
 cd /var/www/html
 sudo bash -c "curl -sS https://getcomposer.org/installer | php"
